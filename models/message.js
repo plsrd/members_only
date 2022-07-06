90e4030e856bc573ffd9cbb5708455fa4d003958
Racheal Pennell <racheal.larimer@gmail.com>
2022-07-06 12:38:57 -0700

    add user and message schema

--- models/message.js
+++ models/message.js
@@ -1,1 +1,22 @@
 const mongoose = require('mongoose');
+const formatDistanceToNow = require('date-fns/formatDistanceToNow');
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, minLength: 1 },
+  created_at: { type: Date, required: true },
+  content: { type: String, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  parent: { type: Schema.Types.ObjectId, ref: 'Message' },
+});
+
+MessageSchema.virtual('url').get(function () {
+  return '/messages/' + this._id;
+});
+
+MessageSchema.virtual('age').get(function () {
+  return formatDistanceToNow(this.created_at);
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const formatDistanceToNow = require('date-fns/formatDistanceToNow');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstname: { type: String, required: true, minLength: 1, maxLength: 25 },
+  lastname: { type: String, required: true, minLength: 1, maxLength: 25 },
+  username: { type: String, required: true, minLength: 1, lowercase: true },
+  password: { type: String, required: true },
+  membership_status: {
+    type: String,
+    required: true,
+    enum: ['user', 'member', 'admin'],
+    default: 'user',
+  },
+  created_at: { type: Date, required: true },
+});
+
+UserSchema.virtual('fullname').get(function () {
+  return this.firstname + ' ' + this.lastname;
+});
+
+UserSchema.virtual('url').get(function () {
+  return '/users/' + this._id;
+});
+
+UserSchema.virtual('age').get(function () {
+  return formatDistanceToNow(this.created_at);
+});
+
+module.exports = mongoose.model('User', UserSchema);
