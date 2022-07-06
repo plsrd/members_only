a54f67150c4727437067063165b9dd68dc6d6ddb
Racheal Pennell <racheal.larimer@gmail.com>
2022-07-06 16:15:11 -0700

    add password hashing and user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const { body, validationResult } = require('express-validator');
+const async = require('async');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
@@ -13,7 +14,13 @@ exports.signup_post = [
     .escape()
     .trim()
     .toLowerCase()
-    .isEmail(),
+    .isEmail()
+    .custom(async email => {
+      const existingUser = await User.findOne({ email: email });
+      if (existingUser) {
+        throw new Error('Email already in use');
+      }
+    }),
   body(
     'password',
     'Password must be at least 8 character, with 1 lowercase & uppercase letter, as well as 1 number & symbol'
@@ -26,39 +33,35 @@ exports.signup_post = [
     .trim()
     .custom(async (passwordConfirm, { req }) => {
       if (req.body.password !== passwordConfirm)
-        throw new Error('Passowords must match');
+        throw new Error('Passwords must match');
     }),
   (req, res, next) => {
     const { firstname, lastname, email, password } = req.body;
 
-    User.findOne({ email }).exec((err, user) => {
-      if (err) return next(err);
+    const errors = validationResult(req);
 
-      const errors = validationResult(req);
-
-      if (user) errors.errors.push({ msg: 'Email already in use' });
-
-      console.log(errors);
-
-      if (!errors.isEmpty()) {
-        res.render('signup', { errors });
-      } else {
-        bcrypt.hash(password, 10, (err, hashedPassword) => {
-          if (err) return next(err);
-
-          new User({
-            firstname,
-            lastname,
-            email,
-            password: hashedPassword,
-            created_at: new Date(),
-          }).save((err, newUser) => {
-            if (err) return next(err);
-            console.log(newUser);
-            res.redirect('/');
-          });
-        });
-      }
+    let user = new User({
+      firstname,
+      lastname,
+      email,
+      password,
     });
+
+    if (!errors.isEmpty()) {
+      res.render('signup', { errors: errors.array(), user });
+    } else {
+      bcrypt.hash(password, 10, (err, hashedPassword) => {
+        if (err) return next(err);
+        Object.assign(user, {
+          password: hashedPassword,
+          created_at: new Date(),
+        });
+
+        user.save((err, savedUser) => {
+          if (err) return next(err);
+          res.redirect('/');
+        });
+      });
+    }
   },
 ];
